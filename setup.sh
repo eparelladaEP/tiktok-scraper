cecbea993cea5c9e9b63d0e2726f3b134fc2ff69
eparelladaEP <enriqueparellada@esta-pasando.com>
2025-03-14 12:20:15 +0100

    Hacer setup.sh ejecutable

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Instalar Playwright y sus dependencias
 playwright install --with-deps
+
+# Forzar instalación de Chromium en el entorno de Streamlit Cloud
 playwright install chromium
 
+# Verificar la instalación de Chromium
+ls -lah /home/appuser/.cache/ms-playwright/
+
+# Mostrar la ubicación de los ejecutables de Playwright
+which playwright
