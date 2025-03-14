9c9d5e1dbc2d2b61e84c3003ee183714e65d6172
eparelladaEP <enriqueparellada@esta-pasando.com>
2025-03-14 12:36:54 +0100

    Añadir dependencias para Playwright en Streamlit Cloud

--- setup.sh
+++ setup.sh
@@ -3,11 +3,8 @@
 # Instalar Playwright y sus dependencias
 playwright install --with-deps
 
-# Forzar instalación de Chromium en el entorno de Streamlit Cloud
+# Instalar Chromium específicamente
 playwright install chromium
-
-# Crear la variable de entorno para forzar modo sin sandbox (Importante en Streamlit Cloud)
-export PLAYWRIGHT_BROWSERS_PATH=0
 
 # Verificar la instalación de Chromium
 ls -lah /home/appuser/.cache/ms-playwright/
